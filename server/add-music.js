0b8b9ee3779aa86fed74385acfa0b28df9a4bff6
Derek Chiang <derekchiang93@gmail.com>
2013-06-29 15:04:02 -0700

    Add songs into DB

---
+++ server/add-music.js
@@ -0,0 +1,23 @@
+var fs = Meteor.require('fs');
+var walk = Meteor.require('walkdir');
+var mm = Meteor.require('musicmetadata');
+// var Fiber = Meteor.require('fibers');
+
+// Meteor.bindEnvironment(function() {
+
+// });
+
+if (Songs.find().count() === 0) {
+  walk('./public/music', Meteor.bindEnvironment(function(path, stat) {
+    var parser = new mm(fs.createReadStream(path));
+    parser.on('metadata', Meteor.bindEnvironment(function(result) {
+      var parts = path.split('/')
+      result['url'] = '/' + parts[parts.length - 2] + '/' + parts[parts.length - 1]
+      Songs.insert(result);
+    }, function(err) {
+
+    }));
+  }, function(err) {
+
+  }));
+}
