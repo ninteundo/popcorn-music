3cfe036ece8e68b809641813e1853be119f1f1ed
Derek Chiang <derekchiang93@gmail.com>
2013-06-29 17:15:30 -0700

    Ignore folders when searching for music

--- server/add-music.js
+++ server/add-music.js
@@ -9,14 +9,20 @@ var mm = Meteor.require('musicmetadata');
 
 if (Songs.find().count() === 0) {
   walk('./public/music', Meteor.bindEnvironment(function(path, stat) {
-    var parser = new mm(fs.createReadStream(path));
-    parser.on('metadata', Meteor.bindEnvironment(function(result) {
-      var parts = path.split('/')
-      result['url'] = '/' + parts[parts.length - 2] + '/' + parts[parts.length - 1]
-      Songs.insert(result);
-    }, function(err) {
+    var parts = path.split('/');
+    // Check if the first character is '.'
+    if ((parts[parts.length - 1].substring(0, 1) == ".") ||
+      (fs.lstatSync(path).isDirectory())) {
+      // Do nothing
+    } else {
+      var parser = new mm(fs.createReadStream(path));
+      parser.on('metadata', Meteor.bindEnvironment(function(result) {
+        result['url'] = '/' + parts[parts.length - 2] + '/' + parts[parts.length - 1]
+        Songs.insert(result);
+      }, function(err) {
 
-    }));
+      }));
+    }
   }, function(err) {
 
   }));
