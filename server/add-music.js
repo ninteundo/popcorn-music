5e53673c1cfd3980708d224713e583f52ca71da0
Simon Last <stlast1@gmail.com>
2013-06-29 21:01:44 -0700

    pretty

--- server/add-music.js
+++ server/add-music.js
@@ -18,6 +18,7 @@ if (Songs.find().count() === 0) {
       var parser = new mm(fs.createReadStream(path));
       parser.on('metadata', Meteor.bindEnvironment(function(result) {
         result['url'] = '/music/' + parts[parts.length - 2] + '/' + parts[parts.length - 1];
+        result.picture = undefined;
         Songs.insert(result);
       }, function(err) {
       }));
